76f1baa08fcd66bdd232d1937bec57d660c053ef
Vitória Moura <vim.saturnino@gmail.com>
2022-07-22 18:23:36 -0300

    chore: database migration

--- db/migrate/20220722205439_create_articles.rb
+++ db/migrate/20220722205439_create_articles.rb
@@ -1,9 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
+    # o create_tablemétodo adiciona uma idcoluna como uma chave primária de incremento automático.
     create_table :articles do |t|
       t.string :title
       t.text :body
 
+      # O método t.timestamps define duas colunas adicionais denominadas created_ate updated_at. 
       t.timestamps
     end
   end
