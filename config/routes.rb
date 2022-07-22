4e0397f1fc48c6bd07adc3927354f61ef8445993
Vitória Moura <vim.saturnino@gmail.com>
2022-07-22 17:38:17 -0300

    chore: add a route that maps the root path of our application to the appropriate controller and action (4.3 Setting the Application Home Page)

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root "articles#index"
+
   get "/articles", to: "articles#index"
 end
